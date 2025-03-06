47b92b64e7b69780b4d12e33953b828ca4ecf5b3
Kod-Draken <m.dackhammar@gmail.com>
2025-03-06 16:06:02 +0100

    functioning gravity and jumping, and a basic game over screen

--- core/src/main/java/se/yrgo/g6/jumpybirb/FirstScreen.java
+++ core/src/main/java/se/yrgo/g6/jumpybirb/FirstScreen.java
@@ -17,13 +17,14 @@ public class FirstScreen implements Screen {
     private Texture background;
     private Texture birb;
     private Texture obstacle;
-
+    private Texture gameOver;
     private SpriteBatch batch;
     private FitViewport viewport;
     private Sprite birbSprite;
+    private Sprite gameOverSprite;
      float velocity = 0;
     final float gravity = -30;
-    final private float jumpStrenght = 100f;
+    final private float jumpStrenght = 500f;
     private float delta;
     private float worldWidth;
     private float worldHeight;
@@ -31,11 +32,14 @@ public class FirstScreen implements Screen {
     public FirstScreen() {
         background = new Texture("placeholder_background.jpg");
         birb = new Texture("placeholder_birb.jpg");
+        gameOver = new Texture("gameover.png");
         //obstacle = new Texture("placeholder_obstacle.jpg");
         batch = new SpriteBatch();
         viewport = new FitViewport(800, 500);
         birbSprite = new Sprite(birb);
         birbSprite.setSize(100, 100);
+        gameOverSprite = new Sprite(gameOver);
+        gameOverSprite.setSize(10, 10);
         worldWidth = viewport.getWorldWidth();
         worldHeight = viewport.getWorldHeight();
         birbSprite.setPosition(worldWidth / 2 -50, worldHeight / 2 -50);
@@ -75,22 +79,42 @@ public class FirstScreen implements Screen {
     }
 
     private void jump() {
-        
-        delta = Gdx.graphics.getDeltaTime();
-
         if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             velocity = jumpStrenght;
-            birbSprite.translateY(jumpStrenght * delta);
         }
     }
 
+    /**
+     * Checks if the player hits the lowest allowed coordinate.
+     * @return true if coordinate is reached, otherwise false.
+     */
+    private boolean isGameOver() {
+        return birbSprite.getY() < -50;
+    }
+
+    /**
+     * For now only draws an ugly "GAME OVER" sprite
+     */
+    private void gameOver() {
+        batch.begin();
+        batch.draw(gameOverSprite,0, 0, worldWidth, worldHeight);
+        batch.end();
+    }
+
     private void logic() {
+        delta = Gdx.graphics.getDeltaTime();
+
+        velocity += gravity;
+        birbSprite.translateY(velocity * delta);
+
         float birdWidth = birb.getWidth();
         float birdHeight = birb.getHeight();
-        birbSprite.setX(MathUtils.clamp(birbSprite.getX(), 0, worldWidth-100));
-        birbSprite.setY(MathUtils.clamp(birbSprite.getY(), 0, worldHeight-100));
 
-        birbSprite.translateY(gravity * delta);
+        birbSprite.setY(MathUtils.clamp(birbSprite.getY(), -100, worldHeight + 100));
+
+        if (isGameOver()) {
+            gameOver();
+        };
     }
 
     @Override
