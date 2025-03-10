a979624bbebc4bebce93435e1b31f76844c0767c
foamel <luohuanyu3124@gmail.com>
2025-03-10 09:31:48 +0100

    Implemented gameOverScreen, simple restart

--- core/src/main/java/se/yrgo/g6/jumpybirb/BirbGame.java
+++ core/src/main/java/se/yrgo/g6/jumpybirb/BirbGame.java
@@ -1,15 +1,24 @@
 package se.yrgo.g6.jumpybirb;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class BirbGame extends Game {
+    private FitViewport viewport = new FitViewport(800, 500);
 
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        newGame();
     }
+
+    public void newGame() {
+        setScreen(new FirstScreen(this, viewport));
+    }
+
+    public void gameOver() {
+        setScreen(new GameOverScreen(this, viewport));
+    }
+
+
 }
